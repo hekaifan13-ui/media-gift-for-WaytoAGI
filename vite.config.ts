f4ff5077ae407cc53849db7e3c8502c1f5a3a678
enter-pro <github@enter.pro>
2026-06-14 07:02:34 +0000

    build: remove unused react plugin from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 import { enterDevPlugin, enterProdPlugin } from 'vite-plugin-enter-dev';
 
 export default defineConfig(({ mode }) => {
@@ -10,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react(), ...enterProdPlugin(), ...enterDevPlugin()],
+      plugins: [...enterProdPlugin(), ...enterDevPlugin()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
